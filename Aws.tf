c57209618ebcd08d567b4203730d402017a57f84
JAI CHAUHAN <131764751+jai-byte@users.noreply.github.com>
2024-07-20 12:24:34 +0530

    Update Aws.tf
    cred deleted

--- Aws.tf
+++ Aws.tf
@@ -1,26 +1,18 @@
-#install terraform on laptop 
-#auth and autherization by terrafrom
-#create a user in AWS AC name terraform
-#attach a policy
-#give adminat privledeges
-#craete sa user
-
-
-
 terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "5.55.0"
+      version = "~> 5.0"
     }
   }
 }
 
+# Configure the AWS Provider
 provider "aws" {
-  # Configuration options
-  region = "us-west-2"
+  region = "us-east-1"
 }
+
 # Create a VPC
-resource "aws_vpc" "VPC1" {
+resource "aws_vpc" "example" {
   cidr_block = "10.0.0.0/16"
 }
